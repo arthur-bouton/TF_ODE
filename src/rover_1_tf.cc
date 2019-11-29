745b94720f6de437fa90f8311dc6c30d5cf59ab9
Arthur <arthur.bouton@gadz.org>
2019-11-29 13:58:49 +0000

    Avoid copy FT sensor tensors twice when shaping the current state

--- src/rover_1_tf.cc
+++ src/rover_1_tf.cc
@@ -68,10 +68,10 @@ p::list Rover_1_tf::GetState( const bool flip ) const
 	state.append( flip_coef*GetRollAngle() );
 	state.append( flip_coef*GetPitchAngle() );
 	state.append( flip_coef*GetBoggieAngle() );
-	Matrix<double,4,3> ft_torsors;
+	const Vector3d* list[] = { _front_ft_sensor.GetForces(), _front_ft_sensor.GetTorques(), _rear_ft_sensor.GetForces(), _rear_ft_sensor.GetTorques() };
 	for ( int i = 0 ; i < 4 ; i++ )
 		for ( int j = 0 ; j < 3 ; j++ )
-			state.append( ( ( i + j )%2 == 0 ? 1 : flip_coef )*ft_torsors( i, j ) );
+			state.append( ( ( i + j )%2 == 0 ? 1 : flip_coef )*list[i][j] );
 	//for ( int i = 0 ; i < NBWHEELS ; i++ )
 		//state.append( _torque_output[i] );
 
