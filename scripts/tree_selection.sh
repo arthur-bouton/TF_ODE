7159530a662b11021ef5e0c64114711ff558588d
Arthur <arthur.bouton@gadz.org>
2020-02-17 12:37:32 +0000

    Add the gathering of all data from the tree selection script in a csv file

--- scripts/tree_selection.sh
+++ scripts/tree_selection.sh
@@ -2,6 +2,7 @@
 
 log_dir=tree_data
 
+
 option_list+="{ oblique: false, max_depth_1: 0, max_depth_2: 0, L1_reg: 0 };"
 option_list+="{ oblique: false, max_depth_1: 0, max_depth_2: 0, L1_reg: 0.01 };"
 option_list+="{ oblique: false, max_depth_1: 0, max_depth_2: 0, L1_reg: 0.1 };"
@@ -74,4 +75,16 @@ for options in $option_list ; do
 	tree_dir=$log_dir/$(echo $options | tr -d ' ')
 	mkdir -p $tree_dir
 	python -u policy_tree.py "$options" $tree_dir/params_ |& tee $tree_dir/log.txt
+done
+
+
+for tree_dir in $log_dir/* ; do
+	csv_line=$(tail -1 $tree_dir/log.txt)
+	if [ $(echo $csv_line | tr -d -c , | wc -c) -eq 13 ] ; then
+		echo $csv_line >> tree_selection.csv
+	else
+		1>&2 echo
+		1>&2 echo Last line in $tree_dir/log.txt does not meet the CSV requirements:
+		1>&2 echo '->' $csv_line
+	fi
 done
