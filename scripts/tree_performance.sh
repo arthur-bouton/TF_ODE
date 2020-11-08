6a3bf1d17cda9d9227fd2bbde968a49886387e08
Arthur Bouton <arthur.bouton@gadz.org>
2020-11-08 23:38:04 +0000

    Add a script to check the performance of each model-tree control with varied step angles and time offsets

---
+++ scripts/tree_performance.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+data_dir=tree_data_mu05_1
+
+csv_file=performance.csv
+
+angle_list=$( seq -1 1 1 )
+offset_list=$( seq 0 0.01 0.09 )
+
+# Position of the trial duration in the string returned from the evaluation:
+duration_pos=2
+
+
+trap 'echo;exit' INT
+for tree_dir in $data_dir/* ; do
+	echo -e '\n\n===' $tree_dir '===\n'
+
+	nsuccesses=0
+	ntrials=0
+	average_time=0
+	for angle in $angle_list ; do
+		for offset in $offset_list ; do
+			result=($(../build/scene_1_mt nodisplay $angle $tree_dir/params_ $offset 2> /dev/null))
+			echo ${result[*]}
+
+			ntrials=$(( ntrials + 1 ))
+			if [[ ${result[*]} == *'[Success]'* ]]; then
+				nsuccesses=$(( nsuccesses + 1 ))
+				average_time=$( bc -l <<< "$average_time + ${result[$duration_pos]}" )
+			fi
+		done
+	done
+
+	success_percentage=0
+	if [ $nsuccesses -ne 0 ] ; then
+		success_percentage=$( echo $nsuccesses $ntrials | awk '{printf "%.2f", $1/$2}' )
+		average_time=$( echo $average_time $nsuccesses | awk '{printf "%.2f", $1/$2}' )
+	fi
+
+	max_depth_1=$( grep -oP '(?<=max_depth_1:).*?(?=,)' <<< $tree_dir )
+	max_depth_2=$( grep -oP '(?<=max_depth_2:).*?(?=,)' <<< $tree_dir )
+	L1=$( grep -oP '(?<=L1:).*?(?=})' <<< $tree_dir )
+
+	echo $max_depth_1,$max_depth_2,$L1,$success_percentage,$average_time >> $data_dir/$csv_file
+done
